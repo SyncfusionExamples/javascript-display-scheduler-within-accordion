da3db73bdf7624fc8ac519e6588411807f85fc71
Uma Maheswari Chandrabose <39375672+UmaMaheswariChandrabose@users.noreply.github.com>
2018-12-11 18:21:31 +0530

    Sample added

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections.Generic;
+using System.Web.Mvc;
+using Syncfusion.EJ2.Navigations;
+
+namespace SyncfusionExample.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            List<AccordionAccordionItem> accItems = new List<AccordionAccordionItem>();
+            accItems.Add(new AccordionAccordionItem { Header = "Click to view Scheduler", Content = "#Current_Enrolments" });
+            ViewBag.accordionItems = accItems;
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
